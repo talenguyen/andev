142db2c4f7df6c1cc388716e794f8e8665168dde
Giang Nguyen <giang.nguyen@tiki.vn>
2018-08-23 09:47:49 +0700

    added device manager to help user can select device

--- src/device-manager.js
+++ src/device-manager.js
@@ -1,52 +1,52 @@
 const exec = require('child_process').exec
-const logger = require('./logger')
+const { errors } = require('./messages')
 const prompts = require('prompts')
 
-class DeviceManager {
-    async selectDevice() {
-        const devices = await this.listDevices()
-        if (devices.length == 0) {
-            return null
+const listDevices = () => (new Promise((resolve, reject) => {
+    exec("adb devices", (error, stdout, stderr) => {
+        if (error) {
+            reject(error)
+        } else {
+            const devices = stdout.split('\n')
+                .filter(line => {
+                    return line !== "List of devices attached" && line.length > 0
+                })
+                .map(line => {
+                    const spaceIndex = line.indexOf("device")
+                    return line.substring(0, spaceIndex).trim()
+                })
+            resolve(devices)
         }
-        if (devices.length == 1) {
-            return devices[0]
-        }
+    })
+}))
 
-        const choices = devices.map(device => ({
-            title: device,
-            value: device
-        }))
+const selectDevice = async () => {
+    const devices = await listDevices()
 
-        const options = {
-            type: 'select',
-            name: 'device',
-            message: 'Pick a device',
-            choices: choices,
-            initial: 0
-        }
-        const response = await prompts(options)
-        return response.device
+    if (devices.length == 0) {
+        throw errors.deviceNotAttached
     }
 
-    listDevices() {
-        return new Promise((resolve, reject) => {
-            exec("adb devices", (error, stdout, stderr) => {
-                if (error) {
-                    reject(error)
-                } else {
-                    const devices = stdout.split('\n')
-                        .filter(line => {
-                            return line !== "List of devices attached" && line.length > 0
-                        })
-                        .map(line => {
-                            const spaceIndex = line.indexOf("device")
-                            return line.substring(0, spaceIndex).trim()
-                        })
-                    resolve(devices)
-                }
-            })
-        })
+    if (devices.length == 1) {
+        return devices[0]
     }
+
+    const choices = devices.map(device => ({
+        title: device,
+        value: device
+    }))
+
+    const options = {
+        type: 'select',
+        name: 'device',
+        message: 'Pick a device',
+        choices: choices,
+        initial: 0
+    }
+    const response = await prompts(options)
+    return response.device
 }
 
-module.exports = DeviceManager
+module.exports = {
+    selectDevice
+}
